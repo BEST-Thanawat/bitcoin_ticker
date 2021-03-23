b0bd7ee990401523affb7ba7f7061d52ec2e69bf
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2021-03-22 23:01:03 -0700

    Add selected currency parameter for CoinAPI.

--- lib/networking.dart
+++ lib/networking.dart
@@ -8,8 +8,8 @@ class NetworkHelper {
 
   final String url;
 
-  Future getData() async {
-    var uri = Uri.parse(url);
+  Future getData(String currency) async {
+    var uri = Uri.parse(url + currency);
     http.Response response = await http.get(uri, headers: {'X-CoinAPI-Key': CoinKEY});
 
     if (response.statusCode == 200) {
